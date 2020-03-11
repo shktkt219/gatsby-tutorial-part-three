1d5924624f5a7675e84de17b2a6f294cd6ccd0d9
shktkt219 <shktkt219@gmail.com>
2020-03-11 04:44:05 +0200

    first commit

---
+++ src/utils/typography.js
@@ -0,0 +1,14 @@
+import Typography from "typography"
+
+const typography = new Typography({
+    bodyFontFamily: [
+        "Alata",
+        "Helvetica Neue",
+        "游ゴシック",
+        "游ゴシック体",
+        "Yu Gothic",
+    ],
+})
+
+export const { scale, rhythm, options } = typography
+export default typography
